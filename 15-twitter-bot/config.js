a67543b7f2133ca387470a0a33b411b168b58d2c
Audrey Moon <audrey@loveme.computer>
2017-10-02 12:56:35 -0500

    rearranged folder structure to match hiercharcy of playlist groups on youtube

--- 15-twitter-bot/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         'Lzt2fVpkYEP0pXCHQtaLZve4H',
-  consumer_secret:      'QPvFSxsHYGhPndKbgy6DnW8pLHY7Qc58wEOwaxIMbS8ZOrDO7U',
-  access_token:         '906249552180674560-hnHODLLff35rMWUWKSiJkMa7TNP3sKV',
-  access_token_secret:  '1N9Yy0xj1AF6vVAlmyEGQapMcOXhvxOvVi1NtMpJrQaXS',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
