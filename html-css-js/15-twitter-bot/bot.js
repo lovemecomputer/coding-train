3c9187ef252009e8d5e296c94297555e1d531ce1
Audrey Moon <audrey@loveme.computer>
2017-10-06 12:07:40 -0500

    some changes from yesterday

--- html-css-js/15-twitter-bot/bot.js
+++ html-css-js/15-twitter-bot/bot.js
@@ -1,5 +1,3 @@
-console.log('the bot is starting')
-
 var Twit = require('twit')
 var config = require('./config')
 
@@ -12,7 +10,17 @@ T.get('search/tweets', params, function(err, data, response) {
   // for(item in data.statuses) {
   //   console.log(item);
   // }
-  data.statuses.forEach(item=>{
-    console.log(item.text);
+  data.statuses.forEach(item => {
+    console.log(item.text)
   })
+})
+
+var newTweet = { status: 'testing node twit~' }
+
+T.post('statuses/update', newTweet, (err, data, response) => {
+  if (err) {
+    console.log(response)
+  } else {
+    console.log('it worked!')
+  }
 })
